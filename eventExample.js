8704e67d112112b671f71bc517d0c715993cfe09
pugnaguido <144164356+pugnaguido@users.noreply.github.com>
2023-11-27 15:57:43 -0700

    Add files via upload

---
+++ eventExample.js
@@ -0,0 +1,78 @@
+/*function assButton() {
+    var inputElement = document.createElement("input");
+    inputElement.setAttribute("type", "button");
+    inputElement.setAttribute("value", "New Button");
+
+    var parent = document.getElementById("memeParagrapgh");
+    parent.appendChild(inputElement);
+
+    var newParagraph  = document.createElement("p");
+
+    var paragraphText = document.createTextNode("New Paragraph");
+
+    newParagraph.appendChild(paragraphText);
+    parent.appendChild(newParagraph);
+}*/
+function addButtons() {
+    console.log("Adding Buttons!");
+
+    let inputElementStart = document.createElement("input");
+    inputElementStart.setAttribute("type", "button");
+    inputElementStart.setAttribute("value", "start");
+    inputElementStart.setAttribute("id", "startButton");
+    inputElementStart.setAttribute("onclick", "startButton()");
+
+
+    let inputElementStop = document.createElement("input");
+    inputElementStop.setAttribute("type", "button");
+    inputElementStop.setAttribute("value", "stop");
+    inputElementStop.setAttribute("id", "stopButton");
+    inputElementStop.setAttribute("onclick", "stopButton()");
+    inputElementStop.setAttribute("disabled", "true");
+
+    let parent = document.getElementById("memeParagraph");
+    parent.appendChild(inputElementStart);
+    parent.appendChild(inputElementStop);
+}
+
+function startButton() {
+    console.log("pressed Sart!");
+    let inputElementStart = document.getElementById("startButton");
+    inputElementStart.setAttribute("disabled", "true");
+
+    let inputElementStop = document.getElementById("stopButton");
+    inputElementStop.removeAttribute("disabled");
+
+    let memeImage = document.getElementById("memeImage")
+    memeImage.style.animationDuration = "3s";
+
+}
+
+function stopButton() {
+    console.log("pressed stop");
+    let inputElementStart = document.getElementById("startButton");
+    inputElementStart.removeAttribute("disabled");
+
+    let inputElementStop = document.getElementById("stopButton");
+    inputElementStop.setAttribute("disabled", "true");
+    
+    let memeImage = document.getElementById("memeImage")
+    memeImage.style.animationDuration = "0s";
+
+}
+
+var clicked = false
+
+window.addEventListener("click", (event) => {
+    if(!clicked){
+        let audioSources = ["drivingrain.wav", "thunderrain.wav", "heavyrain.wav"];
+
+        let audioElement = document.getElementById("backgroundAudio");
+
+        audioElement.src = audioSources[Math.floor(Math.random() * audioSources.length)];
+
+        audioElement.play();
+        audioElement.loop= true;
+        clicked = true
+}
+});
